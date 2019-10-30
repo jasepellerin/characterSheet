af1ca3fd30d562cf374f467e43cf589be3876ee2
Jase Pellerin <jasepellerin@gmail.com>
2019-10-30 15:53:39 -0700

    Testing sign in

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import { Elm } from './elm/Main'
 import './styles/main.scss'
+import netlifyIdentity from 'netlify-identity-widget'
 
-Elm.Main.init()
+// Elm.Main.init()
+
+netlifyIdentity.init()
+netlifyIdentity.open()
