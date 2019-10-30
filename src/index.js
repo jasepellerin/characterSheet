3afaf00fb398c8bdd4b2af2944aaf811396d56b6
Jase Pellerin <jasepellerin@gmail.com>
2019-10-30 16:09:57 -0700

    Basic login

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+import netlifyIdentity from 'netlify-identity-widget'
 import { Elm } from './elm/Main'
 import './styles/main.scss'
-import netlifyIdentity from 'netlify-identity-widget'
-
-// Elm.Main.init()
 
 netlifyIdentity.init()
-netlifyIdentity.open()
+const user = netlifyIdentity.currentUser()
+if (user === null) {
+    netlifyIdentity.open()
+} else {
+    Elm.Main.init()
+}
+
+netlifyIdentity.on('login', () => {
+    netlifyIdentity.close()
+    Elm.Main.init()
+})
