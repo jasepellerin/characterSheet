d40a29ab09f1ae7902d7c021d4167306837bd12d
Jase Pellerin <jasepellerin@gmail.com>
2019-10-30 19:40:55 -0700

    Add the actual route x.x

---
+++ functions/getCharacter.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-console */
+const faunadb = require('faunadb')
+
+const getId = urlPath => urlPath.match(/([^\/]*)\/*$/)[0]
+
+const q = faunadb.query
+const client = new faunadb.Client({
+    secret: process.env.FAUNADB_SERVER_SECRET
+})
+
+exports.handler = async event => {
+    const id = getId(event.path)
+    console.log(`Function 'getCharacter' invoked. Read id: ${id}`)
+    return client
+        .query(q.Get(q.Match(q.Index('by_id', id))))
+        .then(response => {
+            console.log('success', response)
+            return {
+                statusCode: 200,
+                body: JSON.stringify(response)
+            }
+        })
+        .catch(error => {
+            console.log('error', error)
+            return {
+                statusCode: 400,
+                body: JSON.stringify(error)
+            }
+        })
+}
