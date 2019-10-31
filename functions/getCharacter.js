c4af5011176e10065f61f210bf48fd14a431a067
Jase Pellerin <jasepellerin@gmail.com>
2019-10-31 10:35:07 -0700

    Testing

--- functions/getCharacter.js
+++ functions/getCharacter.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 const faunadb = require('faunadb')
-const getId = require('../src/utils/getId')
+const getId = require('./getId')
 
 const fQuery = faunadb.query
 const client = new faunadb.Client({
