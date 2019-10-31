522e29c10a80366d98325a1e1a779c9c6912ae99
Jase Pellerin <jasepellerin@gmail.com>
2019-10-31 09:51:53 -0700

    Trying to figure out redirects

--- src/utils/getId.js
+++ src/utils/getId.js
@@ -1,3 +1,7 @@
-const getId = urlPath => urlPath.match(/([^\/]*)\/*$/)[0]
+// eslint-disable-next-line no-useless-escape
+const getId = urlPath => {
+    console.log(urlPath, urlPath.match(/([^\/]*)\/*$/))
+    return urlPath.match(/([^\/]*)\/*$/)[0]
+}
 
 export default getId
