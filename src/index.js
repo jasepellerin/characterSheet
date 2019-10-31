f0b3b5397f35a167ca8bd473b9b6ac75c6171484
Jase Pellerin <jasepellerin@gmail.com>
2019-10-30 19:37:50 -0700

    Testing actual api route with db call

--- src/index.js
+++ src/index.js
@@ -4,18 +4,17 @@ import './styles/main.scss'
 
 netlifyIdentity.init()
 const user = netlifyIdentity.currentUser()
+console.log(user)
 // if (user === null) {
 //     netlifyIdentity.open()
 // } else {
 //     Elm.Main.init()
 // }
-
 // netlifyIdentity.on('login', () => {
 //     netlifyIdentity.close()
 //     Elm.Main.init()
 // })
-
-fetch('/.netlify/functions/test', {
+fetch('/.netlify/functions/getCharacter/123', {
     method: 'GET'
 }).then(response => {
     return response.json()
